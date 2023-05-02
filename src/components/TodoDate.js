abc88a87c1c7936ecb0150e675dfe3dcd3fcab4b
aleksandra <aleksandra.swigon91@gmail.com>
2023-05-02 22:05:39 +0200

    adding nth function

--- src/components/TodoDate.js
+++ src/components/TodoDate.js
@@ -1,14 +1,37 @@
+import styled from "styled-components";
+
 const TodoDate = ({ date }) => {
-  const day = date.toLocaleString("en-US", { day: "2-digit" });
+  const day = date.toLocaleString("en-US", { day: "numeric" });
   const month = date.toLocaleString("en-US", { month: "long" });
   const year = date.getFullYear();
+
+  const nth = (d) => {
+    if (d > 3 && d < 21) return "th";
+    switch (d % 10) {
+      case 1:
+        return "st";
+      case 2:
+        return "nd";
+      case 3:
+        return "rd";
+      default:
+        return "th";
+    }
+  };
+
   return (
-    <div>
-      <div>{day}</div>
-      <div>{month}</div>
-      <div>{year}</div>
-    </div>
+    <Box>
+      <div>{`${day}${nth(day)} ${month} ${year}`}</div>
+    </Box>
   );
 };
 
 export default TodoDate;
+
+// STYLING SECTION:
+
+const Box = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 0.5rem;
+`;
